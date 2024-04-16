622edc5a91a6cf79b9f68a93ba4596cf4785f967
JailtonJUnior14 <jailtonjrbr@gmail.com>
2024-04-16 11:48:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Despesas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Hello, World");   
+            //}
+            for (int i = 1; i<=9; i++)
+            {
+                Console.Write(i);
+
+                Console.Write("");
+
+                for (int j = 1; j<=9; j++)
+                    
+                {
+                   if (i != j)
+                    {
+                        Console.Write(j);
+                    }
+                        
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
